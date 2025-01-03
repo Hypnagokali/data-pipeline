ba974bc36888dbd662b94b9c94b05252f93a1016
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-01-03 22:57:59 +0100

    init project

---
+++ data-importer/src/importer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def main():
+    # just a test script as placeholder
+    df = pd.DataFrame({"names": ["Bernd", "Sarah", "Hans", "Gundula", "Bertram"],
+                       "values": [7.7, 2.0, 3.0, 4.0, 5.0]})
+
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
